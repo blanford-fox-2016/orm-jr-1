2f548c408f952d3bfd1b2dfc79eea8131625b48f
Juang Wiantoro <juangwiantoro@gmail.com>
2016-10-17 20:35:56 +0700

    all done

--- models/db_model.js
+++ models/db_model.js
@@ -1,8 +1,20 @@
 "use strict"
 
+let sqlite3 = require('sqlite3').verbose();
+let file = new sqlite3.Database('db/init.sql');
+
 class DBModel {
+  constructor() {
+    this.connection = file;
+  }
 
+  createStudents() {
+    this.connection.run("CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT, cohort_id INTEGER)");
+  }
 
+  createCohorts() {
+    this.connection.run("CREATE TABLE cohorts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+  }
 }
 
 export default DBModel
